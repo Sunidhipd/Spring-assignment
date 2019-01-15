aa5c79cd8c50c28b88daa73b4112f6fed9206801
sunidhi.prasad <sunidhi.prasad@boeing.com>
2019-01-15 22:49:02 +0530

    Task 3 Autowiring

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -13,10 +13,19 @@ import org.springframework.core.io.ClassPathResource;
 
 public class Main {
     public static void main(String args[]) {
+        BeanFactory beanFactory=new XmlBeanFactory(new ClassPathResource("beans.xml"));
+        Movie movie1=(Movie) beanFactory.getBean("movie2");
+        System.out.println(movie1.getActor());
+
+        BeanDefinitionRegistry beanDefinitionRegistry=new DefaultListableBeanFactory();
+        BeanDefinitionReader beanDefinitionReader=new XmlBeanDefinitionReader(beanDefinitionRegistry);
+        beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
+        Movie movie2=(Movie)((DefaultListableBeanFactory) beanDefinitionRegistry).getBean("movie2");
+        System.out.println(movie2.getActor());
 
 
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie3 = (Movie) context.getBean("movie3");
+        Movie movie3 = (Movie) context.getBean("movie");
         System.out.println(movie3.getActor());
 
         Movie movie4 = (Movie) context.getBean("movie3");
