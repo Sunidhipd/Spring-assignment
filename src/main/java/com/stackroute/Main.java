67ebfded04a74ff8dc73a367b1793c9dadc878ef
sunidhi.prasad <sunidhi.prasad@boeing.com>
2019-01-15 18:35:54 +0530

    Task-2- Added main brancg contents

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -12,22 +12,32 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String args[]){
 
+        BeanFactory beanFactory=new XmlBeanFactory(new ClassPathResource("beans.xml"));
+        Movie movie1=(Movie) beanFactory.getBean("movie1");
+        System.out.println(movie1.getActor());
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie3 = (Movie) context.getBean("movie3");
+        BeanDefinitionRegistry beanDefinitionRegistry=new DefaultListableBeanFactory();
+        BeanDefinitionReader beanDefinitionReader=new XmlBeanDefinitionReader(beanDefinitionRegistry);
+        beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
+        Movie movie2=(Movie)((DefaultListableBeanFactory) beanDefinitionRegistry).getBean("movie1");
+        System.out.println(movie2.getActor());
+
+        ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+        Movie movie3=(Movie) context.getBean("movie3");
         System.out.println(movie3.getActor());
 
-        Movie movie4 = (Movie) context.getBean("movie3");
+        Movie movie4=(Movie) context.getBean("movie3");
         System.out.println(movie4.getActor());
 
-        System.out.println(movie3 == movie4);
-        Movie movie6 = (Movie) context.getBean("movieA");
+        System.out.println(movie3==movie4);
+        Movie movie6=(Movie) context.getBean("movieA");
         System.out.println(movie6.getActor());
-        Movie movie7 = (Movie) context.getBean("movieB");
+        Movie movie7=(Movie) context.getBean("movieB");
 
         System.out.println(movie7.getActor());
+
     }
 
 }
